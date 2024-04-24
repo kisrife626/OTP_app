2ea00557136997a55744b3b23af255267c0c9a43
kisrife626 <kisrife@harisferi.net>
2024-04-24 21:34:32 +0200

    Empty Lists added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        List<Customer> customers = new ArrayList<>();
+        List<Payments> payments = new ArrayList<>();
+
+
     }
 }
